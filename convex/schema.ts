92e8f8caf84f65f495568a15473a0103c2037747
Mahdad Moshaver <mmoshave@gmu.edu>
2025-04-26 12:08:18 -0400

    Initial commit

---
+++ convex/schema.ts
@@ -0,0 +1,12 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  tasks: defineTable({
+    text: v.string(),
+    isCompleted: v.boolean(),
+  }),
+  numbers: defineTable({
+    value: v.number(),
+  }),
+});
